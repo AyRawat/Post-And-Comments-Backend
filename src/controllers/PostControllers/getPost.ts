00c1b1f9e62fe86f19387573ad7a5864eb615571
AyRawat <aayushrawat20@gmail.com>
2023-05-05 20:06:51 +0530

    Added the Descending filter to get

--- src/controllers/PostControllers/getPost.ts
+++ src/controllers/PostControllers/getPost.ts
@@ -1,7 +1,7 @@
 import db from "db";
 
 export const getPosts = async () => {
-  let query: string = `Select p.id, p.title, p.description,p.created_at, c.body as Comments from posts AS p Left join comments as c ON p.id=c.post_id Order by p.created_at`;
+  let query: string = `Select p.id, p.title, p.description,p.created_at, c.body as Comments from posts AS p Left join comments as c ON p.id=c.post_id Order by p.created_at DESC`;
   return new Promise((resolve, reject) => {
     db.pool.query(query, (err, result) => {
       if (err) {
